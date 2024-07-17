f425c49811d58017a8f6f48975c929026174d110
LorenzoCoppola <lorenzo.coppola@itispascal.it>
2024-07-18 01:00:56 +0200

    modifica degli script GameManager e DialogueManager

--- Game Jam Color is Everything/Assets/Script/DialogueSystem/DialogueManager.cs
+++ Game Jam Color is Everything/Assets/Script/DialogueSystem/DialogueManager.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using Ink.Runtime;
+using TMPro;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    private Story story;
+    private bool dialogueIsPlaying;
+    private static DialogueManager instance;
+    void Awake(){
+        if(instance!=null){
+            Debug.LogWarning("Esiste già un dialogue manager in questa scena");
+        }
+        instance=this;
+    }
+    public static DialogueManager GetInstance(){
+        return instance;
+    }
     void Start()
     {
-        
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+    }
+    public void EnterDialogueMode(TextAsset InkJSON){
+        story = new Story(InkJSON.text);
+        dialogueIsPlaying=true;
+        dialoguePanel.SetActive(true);
+        if(story.canContinue){
+            dialogueText.text= story.Continue();
+        }
+    }
+    public void ExitDialogueMode(){
+        dialogueIsPlaying=false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text="";
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!dialogueIsPlaying)
+        {
+            return;
+        }
         
     }
 }
--- Game Jam Color is Everything/Assets/Script/Game_Manager.cs
+++ Game Jam Color is Everything/Assets/Script/Game_Manager.cs
@@ -7,10 +7,12 @@ public class Game_Manager : MonoBehaviour
     public void Check_Rosso()
     {
         Debug.Log("Ruthlesness is mercy");
+        DialogueManager.GetInstance().ExitDialogueMode();
     }
 
     public void Check_Verde()
     {
         Debug.Log("Greet the world with open arms");
+        DialogueManager.GetInstance().ExitDialogueMode();
     }
 }
