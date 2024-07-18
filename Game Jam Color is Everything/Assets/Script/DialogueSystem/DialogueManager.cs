33ba0dfe7389a6c5f885fa22d4b9c5aa2c3d8b7d
LorenzoCoppola <lorenzo.coppola@itispascal.it>
2024-07-18 23:08:59 +0200

    modifiche dialogueManager

--- Game Jam Color is Everything/Assets/Script/DialogueSystem/DialogueManager.cs
+++ Game Jam Color is Everything/Assets/Script/DialogueSystem/DialogueManager.cs
@@ -1,5 +1,6 @@
 using Ink.Runtime;
 using TMPro;
+using TMPro.Examples;
 using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
@@ -10,6 +11,11 @@ public class DialogueManager : MonoBehaviour
     private Story story;
     private bool dialogueIsPlaying;
     private static DialogueManager instance;
+    [SerializeField] private GameObject[] choices;
+    [SerializeField] private GameObject pressTitle;
+    private TextMeshProUGUI[] choicesText;
+
+
     void Awake(){
         if(instance!=null){
             Debug.LogWarning("Esiste già un dialogue manager in questa scena");
@@ -23,6 +29,13 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
+        pressTitle.SetActive(false);
+        choicesText = new TextMeshProUGUI(dialogueChoices.text);
+        int index=0;
+        foreach(GameObject choices in choices){
+            choicesText[index]= choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
     public void EnterDialogueMode(TextAsset InkJSON){
         story = new Story(InkJSON.text);
@@ -51,6 +64,25 @@ public class DialogueManager : MonoBehaviour
     private void ContinueStory(){
         if(story.canContinue){
             dialogueText.text= story.Continue();
+            DisplayChoices();
         }
+    }
+
+    private void DisplayChoices(){
+        List<Choice> currentChoices = currentStory.currentChoices;
+        if(currentChoices.Count> choices.Lenght){
+            Debug.LogError("Hai superato il massimo di scelte ");
+        }
+        pressTitle.SetActive(true);
+        int index = 0;
+        foreach(Choice choice in currentChoices){
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].Text = choice.text;
+            index++;
+        }
+        for(int i = index; i < choices.Lenght; i++){
+            choices[i].gameObject.SetActive(true);
+        }
+
     }
 }
